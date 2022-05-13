45c13a9d39dff2aa89d5f740ba8e6c645fbe3177
NED-dev-2020 <exaucengango2@gmail.com>
2022-05-13 17:52:47 +0100

    deuxieme

--- main/settings.py
+++ main/settings.py
@@ -102,7 +102,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # information du email
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'cefa0464@gmail.com'
-EMAIL_HOST_PASSWORD = '067027492'
+EMAIL_HOST_PASSWORD = 'obbpbzzadfisfwvm'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
